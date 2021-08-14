07c04bc3d5bf33f093a79c1c01f3e360a6917ef7
xiukun.yuan <xiukun.yuan@quectel.com>
2021-08-14 19:02:37 +0800

    调整代码

---
+++ mainWidget.py
@@ -0,0 +1,22 @@
+from first import  *
+
+
+class MyWindow(QtWidgets.QWidget, Ui_Form):
+    def __init__(self, parent=None):
+        super(MyWindow, self).__init__()
+        self.fileName =''
+        self.setupUi(self)
+        self.btn_push_file_2.clicked.connect(self.msg)
+
+    def msg(self, Filepath):
+        directory = QtWidgets.QFileDialog.getOpenFileName(self, "选取文件", "./", "*.mp3")
+        print('hh ：', directory)
+        fileName = directory[0]
+        self.fileT.setText(fileName)
+
+    def connectPushFile(self,func):
+        self.btn_push_file.clicked.connect(func)
+
+    def getFileName(self):
+        return  self.fileName
+
